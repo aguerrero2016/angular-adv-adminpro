63b441be4dd4d28a7d6a9c87c5f7ae8b3a738fc3
Arturo Martin Guerrero Gutierrez <arturo.guerrero.g@gmail.com>
2022-02-27 12:15:39 -0500

    Fin sección 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
+
+declare function customInitFunctions(): void;
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styles: [
-  ]
+    selector: 'app-pages',
+    templateUrl: './pages.component.html',
+    styles: [
+    ]
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+    
 
-  ngOnInit(): void {
-  }
+    constructor(private settingsService: SettingsService ) { }
+
+    ngOnInit(): void {
+        customInitFunctions();
+    }
 
 }
+
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,14 +9,15 @@ import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ],
+    declarations: [
+        BreadcrumbsComponent,
+        SidebarComponent,
+        HeaderComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     exports: [
         BreadcrumbsComponent,
         SidebarComponent,
