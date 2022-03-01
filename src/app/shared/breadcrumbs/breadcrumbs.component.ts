952f524db6d3e63b4e9340bafb209a9852b63683
Arturo Martin Guerrero Gutierrez <arturo.guerrero.g@gmail.com>
2022-02-28 21:59:34 -0500

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+    selector: 'app-breadcrumbs',
+    templateUrl: './breadcrumbs.component.html',
+    styles: [
+    ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+    public titulo: string = '';
+    public tituloSubs$: Subscription;
+    constructor(private router: Router) { 
+        this.tituloSubs$ = this.getArgumentosRuta();
+    }
+    ngOnDestroy(): void {
+        this.tituloSubs$.unsubscribe();
+    }
 
-  ngOnInit(): void {
-  }
+    getArgumentosRuta(){
+        return this.router.events
+        .pipe(
+            filter<any>( event => event instanceof ActivationEnd ),
+            filter( (event: ActivationEnd) => event.snapshot.firstChild === null),
+            map( (event: ActivationEnd) => event.snapshot.data),
+        )
+        .subscribe( ({ titulo }) => {
+            this.titulo = titulo;
+            document.title = `AdminPro - ${ titulo }`;
+        });
+    }
+
+
 
 }
